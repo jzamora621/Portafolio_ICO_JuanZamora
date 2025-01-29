e612858e66ec6a04a2805f9ec5a7cc8ff1e5012c
jzamora621 <jzamora60021@ufide.ac.cr>
2025-01-28 22:56:15 -0600

    Ejercicios

---
+++ Portafolio_IOC_JuanZamora/src/test/java/Portafolio_IOC_JuanZamora/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_IOC_JuanZamora.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
