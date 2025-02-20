5b41c6e58ce617ec2aafad0828c2036396078217
jzamora621 <jzamora60021@ufide.ac.cr>
2025-02-19 20:46:17 -0600

    Add files via upload

--- Tienda/src/main/java/com/tienda/service/impl/CatergoriaServiceImp.java
+++ Tienda/src/main/java/com/tienda/service/impl/CatergoriaServiceImp.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class CategoriaServiceImp implements CategoriaService {
+public class CatergoriaServiceImp implements CategoriaService {
     
     @Autowired
     private CategoriaDao categoriaDao;
